66475ae8f0c48a13f29928f0aec51ce93c877ef6
nnamdi ezeh <45914831+shagariboy@users.noreply.github.com>
2023-03-06 23:10:17 +0100

    added shopping cart script

---
+++ js/cart.js
@@ -0,0 +1,25 @@
+function addToCart(element) {
+  var productParent = $(element).closest("div.product-item");
+
+  var price = $(productParent).find(".price span").text();
+  var productName = $(productParent).find(".productname").text();
+  var quantity = $(productParent).find(".product-quantity").val();
+
+  var cartItem = {
+    productName: productName,
+    price: price,
+    quantity: quantity,
+  };
+  var cartItemJSON = JSON.stringify(cartItem);
+
+  var cartArray = new Array();
+  // If javascript shopping cart session is not empty
+  if (sessionStorage.getItem("shopping-cart")) {
+    cartArray = JSON.parse(sessionStorage.getItem("shopping-cart"));
+  }
+  cartArray.push(cartItemJSON);
+
+  var cartJSON = JSON.stringify(cartArray);
+  sessionStorage.setItem("shopping-cart", cartJSON);
+  showCartTable();
+}
